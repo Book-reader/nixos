5d8b79ba2fcc42ece42f2315b6fc4760fddcb31b
Book-reader <thevoid@outlook.co.nz>
2025-03-12 15:11:20 +1300

    hate flakes

--- configuration.nix
+++ configuration.nix
@@ -2,10 +2,8 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, nixpkgs, ... }:
+{ config, lib, pkgs, nixpkgs, unstable, ... }:
 let
-	unstable_nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
-	unstable = import unstable_nixpkgs {};
 	locale = "en_NZ.UTF-8";
 	fastfetchPatched = pkgs.fastfetch.overrideAttrs (final: prev: {
 		patches = [ ./pkgs/fastfetch-hyprland.patch ];
@@ -14,6 +12,7 @@ let
 	});
 in
 {
+
 	imports =
 		[ # Include the results of the hardware scan.
 			./hardware-configuration.nix
@@ -274,7 +273,7 @@ in
 	# Copy the NixOS configuration file and link it from the resulting system
 	# (/run/current-system/configuration.nix). This is useful in case you
 	# accidentally delete configuration.nix.
-	system.copySystemConfiguration = true;
+	# system.copySystemConfiguration = true;
 
 	# This option defines the first version of NixOS you have installed on this particular machine,
 	# and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
--- flake.nix
+++ flake.nix
@@ -4,20 +4,25 @@
 	inputs = {
 		# NixOS official package source, using the nixos-24.11 branch here
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+		nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 	};
 
-	outputs = { self, nixpkgs, ... }@inputs: {
+	outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs:
+	let
+		unstable = import nixpkgs-unstable {};
+	in {
 		# Please replace my-nixos with your hostname
 		nixosConfigurations = {
 			NixOS-PC = nixpkgs.lib.nixosSystem {
 				# oh noooo, my reproducibility has been ruined by external variables!!!!! whatever shall I do????
 				system = builtins.currentSystem;
+				specialArgs = { inherit inputs unstable; };
 				modules = [
 					# Import the previous configuration.nix we used,
 					# so the old configuration file still takes effect
 					./configuration.nix
 				];
-		};
+			};
 		};
 	};
 }
