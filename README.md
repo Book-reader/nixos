cc1c22560d5ea62c15e360db956eaaf77195fd50
Book-reader <thevoid@outlook.co.nz>
2025-03-12 23:06:02 +1300

    use flakes more incorrectlier

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ my nixos config.
 
 
 it is truly an awful experience
+
+
+I am aware I am using flakes wrong, I do it out of spite for whoever decided to make these godforsaken flake.nix files standard
--- flake.nix
+++ flake.nix
@@ -5,9 +5,10 @@
 		# Why no workie :(
 		# I have nix 2.27.0pre19700101_dirty
 		# self.submodules = true;
-		# NixOS official package source, using the nixos-24.11 branch here
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-		nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+		# I use this incorrectly out of spite
+		nixpkgs.url = "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
+		nixpkgs-unstable.url = "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
 		# clipboard-sync.url = "path:clipboard-sync";
 	};
 
@@ -16,7 +17,6 @@
 	let
 		unstable = import nixpkgs-unstable {};
 	in {
-		# Please replace my-nixos with your hostname
 		nixosConfigurations = {
 			NixOS-PC = nixpkgs.lib.nixosSystem {
 				# oh noooo, my reproducibility has been ruined by external variables!!!!! whatever shall I do????
